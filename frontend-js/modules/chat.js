b9d0844aacdebd573b344a9ab8ed6dba3d478d02
mtarique <mtarique@outlook.com>
2021-09-14 09:00:53 +0530

    Chat feature completed

--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -1,16 +1,42 @@
+import DOMPurify from "dompurify";
 export default class Chat {
     constructor() {
         this.openedYet = false; 
         this.chatWrapper = document.querySelector("#chat-wrapper"); 
         this.openIcon = document.querySelector(".header-chat-icon"); 
         this.injectHTML(); 
+        this.chatLog = document.querySelector("#chat"); 
+        this.chatField = document.querySelector("#chatField"); 
+        this.chatForm = document.querySelector("#chatForm"); 
         this.closeIcon = document.querySelector(".chat-title-bar-close"); 
         this.events(); 
     }
 
     events() {
+        this.chatForm.addEventListener("submit", (event) => {
+            event.preventDefault(); 
+
+            this.sendMessageToServer(); 
+        }); 
         this.openIcon.addEventListener("click", () => this.showChat())
         this.closeIcon.addEventListener("click", () => this.hideChat())
+    }
+
+    sendMessageToServer() {
+        this.socket.emit('chatMessageFromBrowser', {message: this.chatField.value}); 
+        this.chatLog.insertAdjacentHTML('beforeend', DOMPurify.sanitize(`
+            <div class="chat-self">
+                <div class="chat-message">
+                    <div class="chat-message-inner">
+                        ${this.chatField.value}
+                    </div>
+                </div>
+                <img class="chat-avatar avatar-tiny" src="${this.avatar}">
+            </div>
+        `)); 
+        this.chatLog.scrollTop = this.chatLog.scrollHeight
+        this.chatField.value = ''; 
+        this.chatField.focus(); 
     }
 
     showChat() {
@@ -20,6 +46,7 @@ export default class Chat {
 
         this.openConnection = true; 
         this.chatWrapper.classList.add("chat--visible")
+        this.chatField.focus(); 
     }
 
     hideChat() {
@@ -27,7 +54,31 @@ export default class Chat {
     }
 
     openConnection() {
-        this.socket = io()
+        this.socket = io(); 
+        this.socket.on('welcome', data => {
+            this.username = data.username; 
+            this.avatar = data.avatar; 
+        })
+
+        this.socket.on('chatMessageFromServer', (data) => {
+            this.displayMessageFromServer(data)
+        }); 
+    }
+
+    displayMessageFromServer(data) {
+        this.chatLog.insertAdjacentHTML('beforeend', DOMPurify.sanitize(`
+            <div class="chat-other">
+                <a href="/profile/${data.username}"><img class="avatar-tiny" src="${data.avatar}"></a>
+                <div class="chat-message">
+                    <div class="chat-message-inner">
+                        <a href="/profile/${data.username}"><strong>${data.username}:</strong></a>
+                        ${data.message}
+                    </div>
+                </div>
+            </div>
+        `)); 
+
+        this.chatLog.scrollTop = this.chatLog.scrollHeight
     }
 
     injectHTML() {
